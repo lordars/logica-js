654342f696d8821a7df5ed10894454a16736cf54
Adrián Ricardo Iazbeck Scalia <adrian.r.scalia@gmail.com>
2024-01-18 09:48:43 -0300

    agregando novas forma de resolver

--- app.js
+++ app.js
@@ -1,28 +1,67 @@
-var numSecreto = 29;
+var numSecreto = Math.floor(Math.random() * 100) + 1;
+// OUTRA FORMA DE FAZER O RANDOM
+//var numSecreto = parseInt(Math.random() * 100) + 1;
+var tentativas = 1;
 
-
+console.log(numSecreto);
 
 alert("Boas vindas ao numero secreto");
-NumeroSecreto();
 
-function NumeroSecreto(){
+
+//fazer funcionar por medio de função
+//NumeroSecreto();
+
+
+while(numeroParticipante != numSecreto){
     var numeroParticipante = prompt("Escolha um número entre 1 e 100");
 
     if(numSecreto == numeroParticipante){
-        console.log(`Você acerto o numero secreto: ${numSecreto}`);
+      break;
+        
         
     }else if(numSecreto < numeroParticipante && numeroParticipante != 0 ){
-        console.log(`O numero secreto e menor que : ${numeroParticipante}`);
-        document.querySelector(".container__texto-azul").insette
-
+        alert(`O numero secreto e menor que : ${numeroParticipante} é tua tentantiva: ${tentativas}`);
+        
+        tentativas++
         
     }else if(numSecreto > numeroParticipante && numeroParticipante != 0 ){
-        console.log(`O numero secreto e maior que : ${numeroParticipante}`);
+       alert(`O numero secreto e maior que : ${numeroParticipante} é tua tentantiva: ${tentativas}`);
+       tentativas++
+
     }else{
-        console.log(`Por favor escreve um numero do 1-100 `);
-    }
-    
+        alert(`Por favor escreve um numero do 1-100 `);
+      
+    }   
 }
+
+
+var tentativasSingOrPlural = tentativas>1? "tentativas" : "tentativa";
+alert(`Você acerto o numero secreto: ${numSecreto} conseguiu demorou ${tentativasSingOrPlural}: ${tentativas} `);
+
+
+// function NumeroSecreto(){
+//     var numeroParticipante = prompt("Escolha um número entre 1 e 100");
+
+//     if(numSecreto == numeroParticipante){
+//         alert(`Você acerto o numero secreto: ${numSecreto}`);
+        
+        
+//     }else if(numSecreto < numeroParticipante && numeroParticipante != 0 ){
+//         alert(`O numero secreto e menor que : ${numeroParticipante}`);
+        
+//         NumeroSecreto();
+
+        
+//     }else if(numSecreto > numeroParticipante && numeroParticipante != 0 ){
+//        alert(`O numero secreto e maior que : ${numeroParticipante}`);
+//        NumeroSecreto();
+
+//     }else{
+//         alert(`Por favor escreve um numero do 1-100 `);
+//         NumeroSecreto();
+
+//     }   
+// }
 
 
 
